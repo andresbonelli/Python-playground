808a76f23179c54bf7bd6661388bde9638e545ae
donpipon <donpipon@hotmail.com>
2024-02-23 14:01:35 -0300

    Spiral Matrix - add comments

--- clockwise Spiral Matrix.py
+++ clockwise Spiral Matrix.py
@@ -1,3 +1,21 @@
+#################################################################################
+#                                                                               #
+# Function that generates an "n by n" matrix (list of lists)                    #
+# with spiraling numbers from 1 to "n", starting from center,                   #
+# following clockwise or counter-clockwise sense, ascending or descending.      #
+#                                                                               #
+# example:                                                                      #
+# >>> spiral_matrix(5)                                                          #
+#                                                                               #
+# Expected output:                                                              #
+# [21, 22, 23, 24, 25]                                                          #
+# [20, 7, 8, 9, 10]                                                             #
+# [19, 6, 1, 2, 11]                                                             #
+# [18, 5, 4, 3, 12]                                                             #
+# [17, 16, 15, 14, 13]                                                          #
+#                                                                               #
+#################################################################################
+
 
 def spiral_matrix(size: int, counter_clockwise=False, reverse=False):
 
@@ -63,5 +81,3 @@ def spiral_matrix(size: int, counter_clockwise=False, reverse=False):
     print('\n')
     for row in matrix:
         print(row)
-
-
