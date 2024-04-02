658e68efa4b91b39f06adab4116dc2b216c01a27
donpipon <donpipon@hotmail.com>
2024-04-02 09:23:38 -0300

    Fib Series algorithms, normal dist

---
+++ DistribucionNormal.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+def calcularDistribucionNormal(numbers: list, xlabel: str, ylabel: str):
+    """
+
+    :param numbers: (list of int) lista de valores numéricos
+    :param xlabel: (str) etiqueta de los valores
+    :param ylabel: (str) etiqueta de la densidad por cada valor
+    :return: None
+    """
+    # Creating a DataFrame from array
+    df = pd.DataFrame(numbers)
+
+    # Calculating mean and standard deviation using pandas
+    mean = df.mean().values[0]
+    std_dev = df.std().values[0]
+
+    # Plotting histogram
+    plt.hist(numbers, bins=10, density=True, alpha=0.5, color='grey')
+
+    # Plotting the PDF
+    xmin, xmax = plt.xlim()
+    x = np.linspace(xmin, xmax, 100)
+    p = norm.pdf(x, mean, std_dev)
+    plt.plot(x, p, 'k', linewidth=2)
+    plt.title(f'Distribucion Normal\n'
+              f'Promedio: {round(mean)}, Desviacion: {round(std_dev)}')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+if __name__ == "__main__":
+
+    # Age data
+    edades = [36, 26, 40, 24, 20, 35, 58, 26, 28, 38, 36, 18, 26, 22, 22, 33,
+              39, 29, 54, 26, 43, 20, 39, 40, 20, 25, 40, 26, 22, 32, 62, 34,
+              24, 23, 35, 24, 26, 35, 43, 55, 63, 24, 38, 56, 54, 36, 43]
+
+    calcularDistribucionNormal(edades, xlabel="Edad", ylabel="Densidad")
+
--- FibonacciSeriesGenerator.py
+++ FibonacciSeriesGenerator.py
@@ -1,6 +1,7 @@
+import asyncio
+import time
 
-
-def fibonaci_series_generator(series_length: int, first_num=0, second_num=1):
+def fibonacciSeries(series_length: int, first_num=0, second_num=1):
     """
     Generates a Fibonacci series and computes the golden ratio.
 
@@ -31,12 +32,100 @@ def fibonaci_series_generator(series_length: int, first_num=0, second_num=1):
 
     gold_ratio = series[-1] / series[-2]
 
-    return list(series), gold_ratio
+    return list(series), round(gold_ratio, 3)
+
+
+def recursiveFib(n):
+    """
+    This function generates Fibonacci numbers using recursion.
+    It has exponential time complexity O(2^n) due to repeated calculations.
+    :param n:
+    :return:
+    """
+    try:
+        if n == 0: return 0
+        if n == 1: return 1
+        return recursiveFib(n - 1) + recursiveFib(n - 2)
+    except RecursionError:
+        print(f'[recursiveFib ERROR] - Recursion depth exceeded')
+
+def memoizationFib(n):
+    """
+    This function uses memoization to optimize the recursive Fibonacci.
+    It stores already computed numbers in a dictionary to avoid redundant calculations.
+    The time complexity of this function is O(n), as each Fibonacci number is computed only once.
+    :param n:
+    :return:
+    """
+    try:
+        memo = {0:0, 1:1}
+        def f(n):
+            if n in memo:
+                return memo[n]
+            memo[n] = f(n-1) + f(n-2)
+            return memo[n]
+        return f(n)
+    except RecursionError:
+        print(f'[memoizationFib ERROR] - Recursion depth exceeded')
+        return None
+
+def tabulationFib(n):
+    """
+    This function uses dynamic programming, specifically tabulation, to compute Fibonacci numbers.
+    It iteratively builds up the Fibonacci sequence from the bottom up, storing the results in an array.
+    It has linear time complexity O(n).
+    :param n:
+    :return:
+    """
+    tab = [0, 1]
+    for i in range(2, n+1):
+        new = tab[i-2] + tab[i-1]
+        tab.append(new)
+    return tab[n]
+
+def noMemoFib(n):
+    """
+    This is another iterative approach to computing Fibonacci numbers without memoization.
+    It has the same time complexity as the tabulation and memoization methods, O(n).
+    :param n:
+    :return:
+    """
+    if n < 2: return n
+    a, b = 0, 1
+    for i in range(2, n+1):
+        a, b = b, a + b
+
+    return b
+
+async def measure_time(func, n):
+    """
+    Prints the execution time along with the result from a callback function.
+    :param func: Fibonacci number computing function
+    :param n: The index of the Fibonacci number to compute
+    :return: None
+    """
+    start_time = time.time()
+    result = func(n)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"{func.__name__}({n}) excecution time: {execution_time:.6f} (Result: {result})")
+
+async def main(n):
+    """
+    Runs asynchronous tasks to measure the execution time of various Fibonacci functions.
+    :param n: The index of the Fibonacci number to compute
+    :return: None
+    """
+    tasks = [
+        #measure_time(recursiveFib, n),
+        measure_time(memoizationFib, n),
+        measure_time(tabulationFib, n),
+        measure_time(noMemoFib, n),
+    ]
+    await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
-    n = 20
-    fib_series, golden_ratio = fibonaci_series_generator(n)
-    print(f'Fibonacci series first {n} numbers: {fib_series}')
-    print(f'Golden ratio: {round(golden_ratio, 3)}')
+    n = 40
+    asyncio.run(main(n))
 
 
